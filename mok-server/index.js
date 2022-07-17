86143ab8b097f0a1c1907b8725624352ff5018e5
break8524 <break8524@vaiv.kr>
2022-07-17 21:39:54 +0900

    nest js 대응 업데이트

--- mok-server/index.js
+++ mok-server/index.js
@@ -1,5 +1,4 @@
 var stream = require('getstream');
-// Instantiate a new client (server side)
 client = stream.connect('dhefjeuw9yg5', 'k8n2fjatk2ms5bqr69k4rp732hrtqm6kysw8trv5t8vdq7xd4mqcjb99pt7rbe5x', '1198856');
 var token = client.createUserToken('vaiv');
 console.log(token);
